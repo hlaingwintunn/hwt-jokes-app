2a161432ffee7a74f81979d39f22fa6010f645f8
hlaingwintunn <hlaingwintunn@gmail.com>
2020-09-08 23:22:41 +0800

    added View Layer

--- src/main/java/hwt/springframework/jokes/jokesapp/services/JokeServiceImpl.java
+++ src/main/java/hwt/springframework/jokes/jokesapp/services/JokeServiceImpl.java
@@ -1,7 +1,9 @@
 package hwt.springframework.jokes.jokesapp.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
 
+@Service
 public class JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
