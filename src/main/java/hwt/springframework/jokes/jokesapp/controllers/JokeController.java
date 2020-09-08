df357c05450b0b458f09520b40b998d568b54a35
hlaingwintunn <hlaingwintunn@gmail.com>
2020-09-08 19:20:44 +0800

    Adding Spring MVC Joke Controller

---
+++ src/main/java/hwt/springframework/jokes/jokesapp/controllers/JokeController.java
@@ -0,0 +1,23 @@
+package hwt.springframework.jokes.jokesapp.controllers;
+
+import hwt.springframework.jokes.jokesapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getChuckNorrisQuotes());
+
+        return "chucknorris";
+    }
+}
